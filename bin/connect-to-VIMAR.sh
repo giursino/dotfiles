847bbcdfe9b6e05201849f8334b0f5356cb1e73a
Giuseppe Ursino <giursino@gmail.com>
2020-04-09 17:05:11 +0200

    better vpn

--- bin/connect-to-VIMAR.sh
+++ bin/connect-to-VIMAR.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 set -e
 
+if pgrep snx; then
+  echo "Disconnecting from previous session..."
+  snx -d
+  sleep 1
+fi
+
 echo "Connecting to VIMAR VPN (please insert VIMAR password)..."
 snx -s vpn.vimar.net -u ursinog
 
+echo ""
+echo ""
+echo ""
+echo "*** NOW PLEASE INSERT YOUR SUDO PASSWORD ***"
+sudo -v
+echo "SUDO: OK!"
+echo ""
 echo "Removing 192.168.1.x route from VIMAR VPN..."
 sudo ip route del 192.168.1.1
 sudo ip route del 192.168.1.7
